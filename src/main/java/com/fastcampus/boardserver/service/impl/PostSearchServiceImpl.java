23f983b5b6ca170acebdc5981cd20a6ac1828bcd
heaming <meetoohi@gmail.com>
2024-05-08 12:40:24 +0900

    enh: search API 완료 / Redis 적용 * application.yml * BoardServerApplication.java * build.gradle * post-search-mapper.xml * PostSearchController.java * PostSearchMapper.java * PostSearchRequest.java * PostSearchService.java * PostSearchServiceImpl.java * RedisConfig.java

---
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostSearchServiceImpl.java
@@ -0,0 +1,34 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.PostDTO;
+import com.fastcampus.boardserver.dto.request.PostSearchRequest;
+import com.fastcampus.boardserver.mapper.PostSearchMapper;
+import com.fastcampus.boardserver.service.PostSearchService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+public class PostSearchServiceImpl implements PostSearchService {
+
+    @Autowired
+    private PostSearchMapper mapper;
+
+    @Cacheable(value="getPosts", key="'getPosts' + #request.getName() + #request.getCategoryId()")
+    @Override
+    public List<PostDTO> getPosts(PostSearchRequest request) {
+        List<PostDTO> postDTOS = null;
+
+        try {
+            postDTOS = mapper.selectPosts(request);
+        } catch (RuntimeException e) {
+            log.error("getPosts() 실패:",e.getMessage());
+        }
+
+        return postDTOS;
+    }
+}
